a1fee472be0c72f185d9601c49a505afa1700c18
petter johansson <petter.j@outlook.com>
2024-04-24 09:40:06 +0000

    function: Setting up the Scope and credentials
    Getting access to google sheets and created our "SHEET" variable from the file love_sandwiches in google sheets

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+import gspread #Library for classes and methods
+from google.oauth2.service_account import Credentials #Imports credentials class from service_account function, For google sheets to access our creds.json
+
+#What we want to access
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+#Our credentials to get access
+CREDS = Credentials.from_service_account_file('creds.json')
+#Our credentials applied to only what we want access
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+#Telling gspread our credentials and what we want to access
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+#Creating the sheet from our love_sandwiches file on google sheets
+SHEET = GSPREAD_CLIENT.open('love_sandwiches')
