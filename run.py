6de75fa2129a2d83c11992cccf45f65008788689
petter johansson <petter.j@outlook.com>
2024-04-24 11:00:49 +0000

    function: update_sales_worksheet(data) to update worksheet
    Update sales worksheet, add new row with the list data provided by the user in the terminal

--- run.py
+++ run.py
@@ -23,6 +23,9 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 def get_sales_data():
     """
     Get sales figures input from the user
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers seperated
+    by commas, the loop will repeatedly request data, until it is valid.
     """
     while True:
         print("Please enter sales data from the last market.")
@@ -42,7 +45,7 @@ def get_sales_data():
 
 def validate_data(values):
     """
-    Inside the try, convers all string values into integers.
+    Inside the try, converts all string values into integers.
     Raises ValueError if strings cannot be converted into int,
     or if there aren't exactly 6 values.
     """
@@ -59,6 +62,15 @@ def validate_data(values):
     
     return True
 
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add new row with the list data provided
+    """
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales") #Selects the worksheet
+    sales_worksheet.append_row(data) #Creates a row in the worksheet with the new data
+    print("Sales worksheet updated successfully.\n")
 
-
-data = get_sales_data()
+data = get_sales_data() #data is the users input data
+sales_data = [int(num) for num in data] #sales_data is data but in integers
+update_sales_worksheet(sales_data) #Updates the worksheet with sales_data
